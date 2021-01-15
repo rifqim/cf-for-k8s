42ccb4e0b96e3169956a6fc7da0acd242fafa95c
Jaskanwal Pawar <jpawar@pivotal.io>
2021-01-15 10:57:32 -0800

    wip: fix migration of `install-velero` task from CAKE CI
    Authored-by: Jaskanwal Pawar <jpawar@pivotal.io>

--- ci/tasks/cake/install-velero.sh
+++ ci/tasks/cake/install-velero.sh
@@ -2,7 +2,21 @@
 
 set -euo pipefail
 
-./capi-ci/ci/backup-metadata-generator/helpers/log-into-gke-cluster.bash
+function log_into_gke_cluster() {
+  local ROOT_DIR
+  local project_name
+
+  ROOT_DIR=$PWD
+
+  echo "$GKE_SERVICE_ACCOUNT_KEY" > $ROOT_DIR/service-account.json
+
+  project_name=$(jq -r .project_id $ROOT_DIR/service-account.json)
+
+  gcloud auth activate-service-account --key-file=$ROOT_DIR/service-account.json
+  gcloud container clusters get-credentials "${CLUSTER_NAME}" --zone "${GCP_ZONE}" --project "${project_name}"
+}
+
+log_into_gke_cluster
 
 echo "$VELERO_SERVICE_ACCOUNT_KEY" > velero-service-account.json
 
